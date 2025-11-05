e86eede5d3c98d89777d07e9763db2749916a2c4
mgdejuan <mgdejuan@tip.edu.ph>
2025-11-05 11:09:34 +0800

    .

--- AGSProjects/inventory/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class InventoryAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'inventory'
-
-    def ready(self):
-        # Import signals to ensure they connect when Django starts
-        import inventory.signals
